8c8d6a1da59410862a9634e25bf07a3a64fdef97
tasyamaslova <tasyamaslova@gmail.com>
2025-04-04 10:48:09 +0300

    config.py

--- webapp/app/config.py
+++ webapp/app/config.py
@@ -3,7 +3,7 @@ import os
 SECRET_KEY = b'afd41e94b269e053cc3f6d065a717cffde51ee5208928463ce897faed531006b'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2445_films:04122001@std-mysql.ist.mospolytech.ru/std_2445_films'
+# SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2445_films:04122001@std-mysql.ist.mospolytech.ru/std_2445_films'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 
