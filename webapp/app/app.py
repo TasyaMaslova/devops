4dbf4de5ef46a2dacdaf102bb2c6a5fdba5b86e4
tasyamaslova <tasyamaslova@gmail.com>
2025-04-30 05:09:30 +0300

    feature/tests7

--- webapp/app/app.py
+++ webapp/app/app.py
@@ -64,57 +64,52 @@ def create_app(test_config=None):
 	@login_required
 	def add_comment(id_film):
 	    try:
-		text_comment = request.form['comment']
-		id_parent = request.form.get('id_parent')
-		if id_parent is not None and id_parent.isdigit():
-		    id_parent = int(id_parent)
-		else:
-		    id_parent = None
-		comment = Comments(id_user=current_user.id, id_film=id_film, text_comment=text_comment, id_parent=id_parent)
-		db.session.add(comment)
-		db.session.commit()
+	    	text_comment = request.form['comment']
+	    	id_parent = request.form.get('id_parent')
+	    	if id_parent is not None and id_parent.isdigit():
+	    		id_parent = int(id_parent)
+	    	else:
+	    		id_parent = None
+	    	comment = Comments(id_user=current_user.id, id_film=id_film, text_comment=text_comment, id_parent=id_parent)
+	    	db.session.add(comment)
+	    	db.session.commit()
 	    except SQLAlchemyError as e:
-		db.session.rollback()
-		flash(f'Ошибка при добавлении комментария: {str(e)}', 'danger')
-		return render_template('watch_film', id=id_film)
-
+	    	db.session.rollback()
+	    	flash(f'Ошибка при добавлении комментария: {str(e)}', 'danger')
+	    	return render_template('watch_film', id=id_film)
 	    return redirect(url_for('watch_film', id=id_film))
 
 	# Загрузка кадров фильма
 	@app.route('/download_stills/<int:id_film>', methods=['GET', 'POST'])
 	def download_stills(id_film):
-	    film_record = db.get_or_404(Films, id_film)
-	    stills_directory = os.path.join(app.config['UPLOAD_FOLDER'], 'stills')
-	    
-	    # Создание архива с кадрами
-	    zip_filename = f'stills_{film_record.id}.zip'
-	    with ZipFile(zip_filename, 'w') as zip:
-		for still in film_record.stills:
-		    still_path = os.path.join(stills_directory, still.name_file + '.jpg')
-		    zip.write(still_path, os.path.basename(still_path))
+		film_record = db.get_or_404(Films, id_film)
+		stills_directory = os.path.join(app.config['UPLOAD_FOLDER'], 'stills')
+		# Создание архива с кадрами
+		zip_filename = f'stills_{film_record.id}.zip'
+		with ZipFile(zip_filename, 'w') as zip:
+			for still in film_record.stills:
+				still_path = os.path.join(stills_directory, still.name_file + '.jpg')
+				zip.write(still_path, os.path.basename(still_path))
 
-	    return send_from_directory(directory='.', path=zip_filename, as_attachment=True)
+		return send_from_directory(directory='.', path=zip_filename, as_attachment=True)
 
 	# Добавление записи в таблицу watch_films при нажатии на кнопку "Буду смотреть"
 	@app.route('/add_to_watchlist/<int:id_film>', methods=['POST'])
 	@login_required
 	def add_to_watchlist(id_film):
-	    try:
-		
-		watched_film = db.session.query(WatchedFilms).filter_by(id_film=id_film, id_user=current_user.id).first()
-		if watched_film:
-		    flash('Вы уже добавили этот фильм в список "Буду смотреть".', 'warning')
-		else:
-		    new_watched_film = WatchedFilms(id_film=id_film, id_user=current_user.id)
-		    db.session.add(new_watched_film)
-		    db.session.commit()
-		    flash('Фильм добавлен в список "Буду смотреть".' , 'success')
-	    except SQLAlchemyError as e:
-		db.session.rollback()
-		flash(f'Ошибка при добавлении фильма в список "Буду смотреть": {str(e)}', 'danger')
-		return render_template('watch_film', id=id_film)
-	    
-	    return redirect(url_for('watch_film', id=id_film))
+		try:
+			watched_film = db.session.query(WatchedFilms).filter_by(id_film=id_film, id_user=current_user.id).first()
+			if watched_film:
+				flash('Вы уже добавили этот фильм в список "Буду смотреть".', 'warning')		else:
+				new_watched_film = WatchedFilms(id_film=id_film, id_user=current_user.id)
+				db.session.add(new_watched_film)
+				db.session.commit()
+				flash('Фильм добавлен в список "Буду смотреть".' , 'success')
+		except SQLAlchemyError as e:
+			db.session.rollback()
+			flash(f'Ошибка при добавлении фильма в список "Буду смотреть": {str(e)}', 'danger')
+			return render_template('watch_film', id=id_film)
+		return redirect(url_for('watch_film', id=id_film))
 	    
 	return app
 
