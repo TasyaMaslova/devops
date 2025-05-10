9240dbdce3f55b8da34aac43b2fe681bf5f2b7aa
tasyamaslova <tasyamaslova@gmail.com>
2025-05-11 01:08:03 +0300

    feature/metrics

--- webapp/app/tests.py
+++ webapp/app/tests.py
@@ -1,10 +1,10 @@
 import os
 import pytest
-from app.app import create_app
+from app import create_app
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import Flask, render_template, request, redirect, url_for
 from extensions import db
-from app.models import Users, WatchedFilms, Genres, Films, Stills
+from models import Users, WatchedFilms, Genres, Films, Stills
 
 
 @pytest.fixture
