88be4b92ae09d34ab153ad89f2b7c5716803dbee
tasyamaslova <tasyamaslova@gmail.com>
2025-05-11 02:09:55 +0300

    feature/metrics

--- webapp/app/models.py
+++ webapp/app/models.py
@@ -7,7 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, ForeignKey, Text, MetaData
 from extensions import db
-db.Model.metadata.clear()
 
 class Genres(db.Model):
    __tablename__ = 'genres'
