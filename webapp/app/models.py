ce64e7956ff3933340b65dbe8a33f1d84a70e33f
tasyamaslova <tasyamaslova@gmail.com>
2025-05-10 23:37:16 +0300

    feature/metrics

--- webapp/app/models.py
+++ webapp/app/models.py
@@ -8,14 +8,15 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, ForeignKey, Text, MetaData
 
-class Base(DeclarativeBase):
-  metadata = MetaData(naming_convention={
-        "ix": 'ix_%(column_0_label)s',
-        "uq": "uq_%(table_name)s_%(column_0_name)s",
-        "ck": "ck_%(table_name)s_%(constraint_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s"
-    })
+if 'db' not in globals():
+    class Base(DeclarativeBase):
+      metadata = MetaData(naming_convention={
+            "ix": 'ix_%(column_0_label)s',
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s"
+        })
 
 db = SQLAlchemy(model_class=Base)
 
